578e1c27c20fe4ce9ca9f96e83ee7dccc7c0890c
DESKTOP-2CNEL16\Julen <julenricocrespo@gmail.com>
2025-01-08 23:59:36 +0100

    Add LoadScene and Exit functions for menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuController
+public class MainMenuController : MonoBehaviour
 {
-    
+    // Method to load a specific minigame scene
+    public void LoadMinigame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Method to exit the game
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        // Stops play mode in the Unity Editor
+        UnityEditor.EditorApplication.isPlaying = false; 
+#else
+            // Closes the application in a built version
+            Application.Quit(); 
+#endif
+    }
 }
